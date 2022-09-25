c65f23c93b432e7c710fedc5a8694386e330632d
IKE37 <92145145+IKE37@users.noreply.github.com>
2022-09-26 02:24:44 +0700

    tao them Enemy

---
+++ Assets/bomController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomController : MonoBehaviour {
+
+	public float bomSpeedHight;
+	public float bomSpeedLow;
+	public float bomAngle;
+
+	Rigidbody2D bomRB;
+
+	void Awake(){
+		bomRB = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		bomRB.AddForce (new Vector2 (Random.Range (-bomAngle, bomAngle), Random.Range (bomSpeedLow, bomSpeedHight)), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/undeadShoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class undeadShoot : MonoBehaviour {
+
+	public GameObject theBom;
+	public Transform shootForm;
+	public float shootTime;
+
+	float nextShoot = 0f;
+	Animator undeadAnim;
+
+	void Awake(){
+		undeadAnim = GetComponentInChildren<Animator> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.tag == "Player" && Time.time > nextShoot) {
+			nextShoot = Time.time + shootTime;
+			Instantiate (theBom, shootForm.position, Quaternion.identity);
+			undeadAnim.SetTrigger ("undeadShoot");
+		}
+	}
+}
